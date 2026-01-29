db30549283bea92e4e61017cdd96cf58cd09d307
M Aun Rasheed <greataun786@gmail.com>
2026-01-29 22:18:53 +0500

    feat; Dynamic Product Page updated

--- app/product/[slug]/page.js
+++ app/product/[slug]/page.js
@@ -1,4 +1,5 @@
 'use client'
+import ProductCard from '@/components/ProductCard'
 import ProductDetailCard from '@/components/ProductDetailCard'
 import { ProductContext } from '@/components/ProductProvider'
 import React, { useContext } from 'react'
@@ -13,12 +14,26 @@ const ProductDetail = ({ params }) => {
 
     if (!details) return <div className='font-body font-bold text-center p-10 text-4xl'>Loading...</div>;
 
+    const relatedProducts = product.filter(item => {
+        return (item.slug != slug && item.category.title === details.category.title)
+    })
+
     return (
         <>
             <main className='max-w-[1600px] mx-auto'>
 
                 <section>
                     <ProductDetailCard product={details} />
+                </section>
+
+                <section className='px-16 mt-10 h-fit py-10'>
+                    <h2 className='text-4xl font-bold font-body underline'>Other relevant products :</h2>
+                    {relatedProducts.length === 0 && <div className='font-body font-semibold text-center p-10 text-xl'>No related product found.</div>}
+                    <div className='py-7 w-fit'>
+                        {relatedProducts.map(item => {
+                            return <ProductCard key={item.id} product={item} />
+                        })}
+                    </div>
                 </section>
 
             </main>
